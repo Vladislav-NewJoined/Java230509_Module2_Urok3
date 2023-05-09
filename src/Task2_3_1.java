ff47d65563ad371f58084c94a65fe6607f15b189
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-05-09 20:03:28 +0300

    Initial commit

---
+++ src/Task2_3_1.java
@@ -0,0 +1,20 @@
+public class Task2_3_1 {
+
+    public static void main(String[] args) {
+        System.out.println("Задание: \n1.\tВыведите числа от двух в десятой степени до десяти в четвертой степени\n\nРешение: ");
+
+        int numBasis = 2;
+        int numExponent = 10;
+        int result = (int) Math.pow(numBasis, numExponent);
+        /*System.out.println(result);*/
+
+        int numBasis2 = 10;
+        int numExponent2 = 4;
+        int result2 = (int) Math.pow(numBasis2, numExponent2);
+        /*System.out.println(result2);*/
+
+        for (int i = result; i <= result2; i++) {
+            System.out.println(i);
+        }
+    }
+}
