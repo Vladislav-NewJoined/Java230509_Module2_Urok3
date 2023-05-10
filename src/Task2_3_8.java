783d2b72af9e1ab4ca6b198dbceb4894364df519
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-05-10 16:10:33 +0300

    Initial commit

--- src/Task2_3_8.java
+++ src/Task2_3_8.java
@@ -1,13 +1,11 @@
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-
 // Видеоурок здесь: https://lms.synergy.ru/learning/view/68493
 // Задание здесь: https://lms.synergy.ru/lntools/mcresource/view/67098/?groupPeriodId=1045153&disciplineVersionId=25858&start=1
 public class Task2_3_8 {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         System.out.println("Задание: \n8.\tвыведите греческий алфавит (коды с 945 по 969) через запятую (а, в, •..)\n\nРешение: ");
+        for (int i = 945; i<=969; i++) {
+            System.out.print((char)i + ", ");
+        }
     }
 }
